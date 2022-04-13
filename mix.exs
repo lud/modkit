814e2deff6e8423a0d5f922085b998b8b21f6f88
lud <ludovic@demblans.com>
2022-04-13 18:34:20 +0200

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Modkit.MixProject do
   def project do
     [
       app: :modkit,
-      version: "0.1.0",
+      version: "0.1.1",
       description: "A set of tool to work with Elixir modules files.",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
